6c96aa1c3a6106e603229f0cc38e3751aa3966c9
kiddocoder <ndayikezatresor9@gmail.com>
2024-08-05 21:44:19 +0200

    change admin

---
+++ admin/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+<?php
+    include 'includes/heade.php';
+  ?>
+<body>
+
+  <!-- ======= Header ======= -->
+  <?php
+    include 'includes/header.php';
+  ?>
+  <!-- End Header -->
+
+  <!-- ======= Sidebar ======= -->
+  <?php
+    include 'includes/siderbar.php';
+  ?>
+  <!-- End Sidebar-->
+
+  <?php
+    include 'includes/main.php';
+  ?>
+  <!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <?php
+    include 'includes/footer.php';
+  ?>
+  <!-- End Footer -->
+  <?php
+    include 'includes/js-include.php';
+  ?>
+
+</body>
+
+</html>

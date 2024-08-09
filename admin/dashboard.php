3d2fe519d1dbe3528f3b54badbecd124c2f728b9
kiddocoder <ndayikezatresor9@gmail.com>
2024-08-09 23:42:05 +0200

    update

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -162,53 +162,32 @@ include(Root_path."includes/siderbar_admin.php");
             <div class="col-12">
               <div class="card recent-sales overflow-auto">
                 <div class="card-body">
-                  <h5 class="card-title">Recent Sales <span>| Today</span></h5>
+                  <h5 class="card-title">Recent Languages</h5>
 
                   <table class="table table-borderless datatable">
                     <thead>
                       <tr>
                         <th scope="col">#</th>
-                        <th scope="col">Customer</th>
-                        <th scope="col">Product</th>
-                        <th scope="col">Price</th>
-                        <th scope="col">Status</th>
+                        <th scope="col">languages</th>
+                        <th scope="col">Niveau(%)</th>
+                        <th scope="col">Delete</th>
+                        <th scope="col">Update</th>
                       </tr>
                     </thead>
                     <tbody>
                       <tr>
                         <th scope="row"><a href="#">#2457</a></th>
-                        <td>Brandon Jacob</td>
-                        <td><a href="#" class="text-primary">At praesentium minu</a></td>
-                        <td>$64</td>
-                        <td><span class="badge bg-success">Approved</span></td>
-                      </tr>
-                      <tr>
-                        <th scope="row"><a href="#">#2147</a></th>
-                        <td>Bridie Kessler</td>
-                        <td><a href="#" class="text-primary">Blanditiis dolor omnis similique</a></td>
-                        <td>$47</td>
-                        <td><span class="badge bg-warning">Pending</span></td>
-                      </tr>
-                      <tr>
-                        <th scope="row"><a href="#">#2049</a></th>
-                        <td>Ashleigh Langosh</td>
-                        <td><a href="#" class="text-primary">At recusandae consectetur</a></td>
-                        <td>$147</td>
-                        <td><span class="badge bg-success">Approved</span></td>
+                        <td>French</td>
+                        <td><a href="#" class="text-primary">90</a></td>
+                        <td><button>X</button></td>
+                        <td><span class="badge bg-success">Update</span></td>
                       </tr>
                       <tr>
                         <th scope="row"><a href="#">#2644</a></th>
-                        <td>Angus Grady</td>
-                        <td><a href="#" class="text-primar">Ut voluptatem id earum et</a></td>
-                        <td>$67</td>
-                        <td><span class="badge bg-danger">Rejected</span></td>
-                      </tr>
-                      <tr>
-                        <th scope="row"><a href="#">#2644</a></th>
-                        <td>Raheem Lehner</td>
-                        <td><a href="#" class="text-primary">Sunt similique distinctio</a></td>
-                        <td>$165</td>
-                        <td><span class="badge bg-success">Approved</span></td>
+                        <td>English</td>
+                        <td><a href="#" class="text-primary">50</a></td>
+                        <td><button>X</button></td>
+                        <td><span class="badge bg-success">Update</span></td>
                       </tr>
                     </tbody>
                   </table>
@@ -217,7 +196,7 @@ include(Root_path."includes/siderbar_admin.php");
 
               </div>
             </div><!-- End Recent Sales -->
-            
+
           </div>
         </div><!-- End Left side columns -->
       </div>

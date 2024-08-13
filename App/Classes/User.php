cb5792c80a85ed3f3d02da2066109485c5c49643
kiddocoder <ndayikezatresor9@gmail.com>
2024-08-13 15:52:31 +0200

    update

---
+++ App/Classes/User.php
@@ -0,0 +1,77 @@
+<?php
+namespace App\classes;
+
+include("../../functions/query.func.php");
+use App\classes\DB;
+use App\classes\Cookie;
+use App\classes\Session;
+
+class User {
+
+      private $db,$conn,$sessions,$cookies;
+      public function __construct() {
+           $this->db = new DB();
+           $this->sessions = new Session();
+           $this->cookies = new Cookie();
+
+           $this->conn = $this->db->Connexion();
+      }
+
+      /**
+       * @param $email user email
+       * @param $password user password
+       * @return array
+       */
+      function checkUser($email,$password) : array{
+
+            if (empty($email) && empty($password)) {
+                  $data['errorlogin']['message'] = 'Adresse email ou mot de passe invalide.';
+                  exit;
+              }
+
+              $email = htmlspecialchars($_POST['email']);
+              $password = htmlspecialchars($_POST['mdp']);
+
+              $emailCheck = "SELECT * FROM utilisateurs WHERE email = ?";
+              $result = runQuery($emailCheck, $email);
+
+              if ($result->rowCount() > 0) {
+                  $user = $result->fetch();
+                  if (password_verify($password, $user['mot_de_passe'])) {
+                      $data['loginsuccess']['message'] = 'Login successful!';
+
+                      // start session
+                      $this->sessions->startSession();
+
+                      // set session variables
+                      $this->sessions->setSession('uid', $user['uid']);
+                      $this->sessions->setSession('email', $user['email']);
+                      $this->sessions->setSession('photo', $user['photo']);
+
+                      // set cookie for one year
+                      $this->cookies->setCookie('uid', $user['uid'], 3600*24*60);
+                      $this->cookies->setCookie('email', $user['email'], 3600*24*60);
+                      $this->cookies->setCookie('photo', $user['profile'], 3600*24*60);
+
+                  } else {
+                      $data['errorlogin']['message'] = 'Mot de passe incorrect.';
+                  }
+              } else {
+                  $data['errorlogin']['message'] = 'Aucun compte avec cette adresse email.';
+              }
+      }
+
+      function insertUser($data){
+
+      }
+
+      function deleteUser($id){
+
+      }
+
+      function logout(){
+
+      }
+
+
+}
--- login.php
+++ login.php
@@ -1,8 +1,52 @@
 <?php
-include("./root.php");
+
+  if(isset($_SESSION['user'])){
+    echo "<script type='text/javascript'> document.location = 'index.php?page=dashboard'; </script>";
+  }
+
+  function utilisateur($email,$password)
+  {
+
+  }
+  if(isset($_SESSION['user'])){
+    echo "<script type='text/javascript'> document.location = 'index.php?page=cv'; </script>";
+  }
+
+  if(isset($_POST['connexion'])){
+    $email = htmlspecialchars(trim($_POST['email']));
+    $password = htmlspecialchars(trim($_POST['mdp']));
+
+    $errors = [];
+
+    if(empty($email) &&  empty($password)){
+        $errors['empty'] = "Entre votre Mot de passe ou l'email !";
+    }else if(utilisateur($email,$password) == 0 ){
+        $errors['exist']  = "Ce compte n'existe pas ou vous avez oublier un champ à completer";
+    }
+
+    if(!empty($errors)){
+        ?>
+          <div class="card gb-danger">
+            <div class="card-content gb-danger">
+              <h6 class="gb-danger text-danger" >
+                <?php
+                    foreach($errors as $error){
+                        echo $error."<br/>";
+                    }
+                ?>
+              </h6>
+            </div>
+          </div>
+        <?php
+    }
+    else{
+      $_SESSION['user'] = $email;
+      // echo "<script type='text/javascript'> document.location = 'index.php?page=cv'; </script>";
+    }
+  }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -22,14 +66,14 @@ include("./root.php");
         <div class="card" style="border-radius: 1rem;">
           <div class="row g-0">
             <div class="col-md-6 col-lg-5  d-none d-md-block">
-               <img class="img-fluid flex fill" style="border-radius: 1rem 0 0 1rem;" src="<?=base_url?>public/images/cv-write.jpeg" alt="cv">
+               <img class="img-fluid flex fill" style="border-radius: 1rem 0 0 1rem;" src="public/images/cv-write.jpeg" alt="cv">
             </div>
             <div class="col-md-6 col-lg-7 d-flex align-items-center">
               <div class="card-body p-4 p-lg-5 text-black">
 
-                <form action="<?=base_url?>validators/checkLogin.php" method="POST" id="login-form">
+                <form  method="POST">
 
-                  <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Connectez-vous à votre compte</h5>
+                  <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Connectez-vous à votre compte </h5>
                   <div  class="form-outline mb-4">
                     <label class="form-label" for="email">Adresse email</label>
                     <input type="email" id="email" name="email" class="form-control form-control-lg" placeholder="janedoe@gmail.com" required/>
@@ -43,11 +87,13 @@ include("./root.php");
                   </div>
 
                   <div class="pt-1 mb-4">
-                    <button  type="submit" class="btn btn-primary btn-lg w-100">Login</button>
+                    <button name="connexion" type="submit" class="btn btn-primary btn-lg w-100">Login</button>
                   </div>
 
-                  <p class="mb-5 pb-lg-2" style="color: #393f81;">Don't have an account? <a href="<?=base_url?>signup.php"
-                      style="color: #393f81;">Register here</a></p>
+                  <p class="mb-5 pb-lg-2" style="color: #393f81;">J'ai pas de compte ? <a href="index.php?page=signup"
+                      style="color: #393f81;">Inscription</a>
+                    </p>
+
                 </form>
 
               </div>
@@ -59,10 +105,6 @@ include("./root.php");
   </div>
 </section>
 
-<!-- Scripts -->
-<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
-<script src="<?= base_url?>public/js/utils.js"></script>
-<script src="<?= base_url?>public/js/auth.js"></script>
 </body>
 </html>
